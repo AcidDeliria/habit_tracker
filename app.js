ed621eabac92ceb46f8c17098241aca41bff1234
Aleksandra <aciddeliria@Aleksandras-MBP.lan>
2020-10-28 20:41:15 +0100

    fixed selected days local storage

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const dateElement = document.getElementById("date");
 const alertPopup = document.getElementById('alertmain');
 const closeAlert = document.getElementById('closebtn');
 let colorChoice = document.getElementById('habit-color').value;
-
-//TODO : localStorage.clear();
+let week = [false, false, false, false, false, false, false]
 
 let HABITLIST;
 let id=0;
@@ -20,7 +19,7 @@ const LOCAL_STORAGE_KEY = "habit"
 function loadHabit(array){
         array.forEach(function(habit){
            if(!habit.remover){
-            addRow(habit.name, habit.id, habit.color)
+            addRow(habit.name, habit.id, habit.color, habit.week)
            }
         
     })
@@ -34,8 +33,8 @@ function save(){
 }
 //habit object creator
 
-function createHabit(name, colorChoice) {
-    return { name: name, id: id, color: colorChoice, remover: false }
+function createHabit(name, colorChoice, week) {
+    return { name: name, id: id, color: colorChoice, week: week, remover: false }
   }
 
 
@@ -57,36 +56,47 @@ const options = {weekday : "long", month : "short", day : "numeric"}
 const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("en-US", options)
-
 document.getElementById("habit-color").onchange = function() {
     backHEX = this.value;
     colorChoice = backHEX;
 };
 
+ //color coding buttons
 
+document.addEventListener('click', handleClick)
+function handleClick(event){
+const element = event.target
+    if (element.classList.contains('day-button')) {
+        element.style.backgroundColor = element.parentNode.id;
+        let habit = HABITLIST.find(function(habit){
+            return habit.id == element.parentNode.dataset.habitId
+        })
+        if (element.classList.contains('selected')) {
+            habit.week[element.dataset.day] = false;
+            element.classList.remove('selected') //this true 
+            element.style.backgroundColor = "#E7E7E7";
+          } else {
+            habit.week[element.dataset.day] = true;
+          element.classList.add('selected')
+          element.style.backgroundColor = element.parentNode.id;
+        }
 
-// color coding buttons
-
-document.addEventListener('click', function(event){
-const element=event.target;
-if (element.classList.contains('day-button')) {
-    element.style.backgroundColor = element.parentNode.id;
-    if (element.classList.contains('selected')) {
-        element.classList.remove('selected')
-        element.style.backgroundColor = "#E7E7E7";
-      } else {
-      element.classList.add('selected')
-      element.style.backgroundColor = element.parentNode.id;
+        console.log("day", element.dataset.day)
+        console.log("habitId", element.parentNode.dataset.habitId)
+        console.log("habit", habit)
+        }
+        
+      save() 
     }
-}})
 
 
 
 // add habit (row + push to array and local storage)
 
  function addHabit(){
-    const habit = createHabit(document.getElementById('formfield').value, colorChoice)
-    addRow(habit.name, habit.id, habit.color);
+    const habit = createHabit(document.getElementById('formfield').value, colorChoice,[false, false, false, false, false, false, false] )
+    addRow(habit.name, habit.id, habit.color, habit.week);
+    //handleClick()
     HABITLIST.push(habit);
     id++;
      save();
@@ -105,7 +115,7 @@ if (element.classList.contains('day-button')) {
 
 //create row and insert it to HTML
 
-function addRow(inputText, id , colorChoice) {
+function addRow(inputText, id , colorChoice, week) {
     if (inputText!='') {
         const row = 
         `
@@ -117,14 +127,14 @@ function addRow(inputText, id , colorChoice) {
 
             <div class="habitname ib"  style = background-color:${colorChoice}><p>${inputText}</p></div>
 
-            <div class="row row-yellow" id = ${colorChoice}>
-                <button class="day-button">Mon</button>
-                <button class="day-button">Tues</button>
-                <button class="day-button">Wed</button>
-                <button class="day-button">Thurs</button>
-                <button class="day-button">Fri</button>
-                <button class="day-button">Sat</button>
-                <button class="day-button">Sun</button>
+            <div class="row row-yellow" id = ${colorChoice} data-habit-id="${id}">
+                <button class="day-button ${week[0] && "selected"}" data-day="0" ${week[0] &&  `style = background-color:${colorChoice}`} >Mon</button>
+                <button class="day-button ${week[1] && "selected"}" data-day="1" ${week[1] &&  `style = background-color:${colorChoice}`}>Tues</button>
+                <button class="day-button ${week[2] && "selected"}" data-day="2" ${week[2] &&  `style = background-color:${colorChoice}`}>Wed</button>
+                <button class="day-button ${week[3] && "selected"}" data-day="3" ${week[3] &&  `style = background-color:${colorChoice}`}>Thurs</button>
+                <button class="day-button ${week[4] && "selected"}" data-day="4" ${week[4] &&  `style = background-color:${colorChoice}`}>Fri</button>
+                <button class="day-button ${week[5] && "selected"}" data-day="5" ${week[5] &&  `style = background-color:${colorChoice}`}>Sat</button>
+                <button class="day-button ${week[6] && "selected"}" data-day="6" ${week[6] &&  `style = background-color:${colorChoice}`}>Sun</button>
             </div>
         </div>  
          
@@ -136,7 +146,7 @@ function addRow(inputText, id , colorChoice) {
     else{
         addAlert();
     }
-
+console.log(week)
 }
 
   //remove button
