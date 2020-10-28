70911b50b4e10a686af5300d4dbfd6fb5411ed39
hownbashi <42399764+hownbashi@users.noreply.github.com>
2020-10-28 16:47:37 +0100

    same as master branch

---
+++ README.md
@@ -0,0 +1,4 @@
+# habit_tracker
+Aleksandra, Hossein and Paul's Habit Tracker Repository
+
+A simple weekly habit tracker, where you can add habits, fill in bubbles for each day completed and delete. You can use the enter key!
--- app.js
+++ app.js
@@ -1,10 +1,62 @@
-// var dayButtons = [].slice.call(document.querySelectorAll('.day-button'));
-const habit = document.getElementById("habit-name");
-const trash = document.querySelector("#trash");
 const list = document.getElementById("container1");
-const addButton = document.getElementById(".add-btn")
+const addButton = document.getElementById("addButton")
 const dateElement = document.getElementById("date");
-const task = formfield.value;
+const alertPopup = document.getElementById('alertmain');
+const closeAlert = document.getElementById('closebtn');
+let colorChoice = document.getElementById('habit-color').value;
+const clearBtn = document.getElementById('clearBtn');
+
+
+//remove button, and local storage clear;
+
+clearBtn.addEventListener('click',function (){
+    localStorage.clear()
+    document.getElementsByClassName('row-wrapper').remove()
+})
+
+let HABITLIST;
+let id=0;
+const LOCAL_STORAGE_KEY = "habit"
+ HABITLIST = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || []
+     id = HABITLIST.length; 
+     loadHabit(HABITLIST);
+
+
+//load items from local storage
+
+function loadHabit(array){
+        array.forEach(function(habit){
+           if(!habit.remover){
+            addRow(habit.name, habit.id, habit.color)
+           }
+        
+    })
+}
+          
+//saves items in the local storage
+
+function save(){
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(HABITLIST))
+    
+}
+//habit object creator
+
+function createHabit(name, colorChoice) {
+    return { name: name, id: id, color: colorChoice, remover: false }
+  }
+
+
+//No habit text alert popup function
+
+closeAlert.addEventListener('click' , removeAlert)
+
+function removeAlert() {
+    alertPopup.classList.add('dis')
+}
+
+function addAlert () {
+    alertPopup.classList.remove('dis')
+}
 
 //Date Element
 
@@ -13,41 +65,66 @@ const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("en-US", options)
 
+document.getElementById("habit-color").onchange = function() {
+    backHEX = this.value;
+    colorChoice = backHEX;
+};
+
+
+
 // color coding buttons
 
-// dayButtons.forEach(button => button.addEventListener(('click'), function () {
-//   if (button.classList.contains('selected')) {
-//         button.classList.remove('selected')
-//       } else {
-//       button.classList.add('selected')
-//   }
-// }))
-document.addEventListener('click', function (event){
+document.addEventListener('click', function(event){
 const element=event.target;
 if (element.classList.contains('day-button')) {
+    element.style.backgroundColor = element.parentNode.id;
     if (element.classList.contains('selected')) {
         element.classList.remove('selected')
+        element.style.backgroundColor = "#E7E7E7";
       } else {
       element.classList.add('selected')
-  }
-    
-}
-})
-// Function add Habit
+      element.style.backgroundColor = element.parentNode.id;
+    }
+}})
 
-document.getElementById('addButton').addEventListener('click', function (){
-    const inputText=document.getElementById('formfield').value;
+
+
+// add habit (row + push to array and local storage)
+
+ function addHabit(){
+    const habit = createHabit(document.getElementById('formfield').value, colorChoice)
+    addRow(habit.name, habit.id, habit.color);
+    HABITLIST.push(habit);
+    id++;
+     save();
+ }
+
+//add habit on click and enter
+
+ addButton.addEventListener('click', addHabit);
+ document.addEventListener('keypress', function (e){
+        if (e.key === 'Enter') {
+            e.preventDefault()
+        addHabit()
+ }})
+
+
+
+//create row and insert it to HTML
+
+function addRow(inputText, id , colorChoice) {
     if (inputText!='') {
-        
-        const row = `
+        const row = 
+        `
+
         <div class="row row-wrapper">
-            <button class="removebutton" onclick="remover(this)">
+            <button class="removebutton" onclick="remover(this, ${id})">
                 <i id="trash" class="far fa-trash-alt"></i>
             </button>
 
-            <div class="habitname ib"><p>${inputText}</p></div>
+            <div class="habitname ib"  style = background-color:${colorChoice}><p>${inputText}</p></div>
 
-            <div class="row  row-yellow">
+            <div class="row" id = ${colorChoice}>
                 <button class="day-button">Mon</button>
                 <button class="day-button">Tues</button>
                 <button class="day-button">Wed</button>
@@ -56,29 +133,22 @@ document.getElementById('addButton').addEventListener('click', function (){
                 <button class="day-button">Sat</button>
                 <button class="day-button">Sun</button>
             </div>
-        </div>   
+        </div>  
+         
         `
     const position = "beforeEnd"
     list.insertAdjacentHTML(position, row)
     document.getElementById('formfield').value='';      
     }
     else{
-        alert('Please Insert Habit Name');
+        addAlert();
     }
-  
-})
-function remover(element) {
-    element.parentElement.remove();  
+
 }
-document.getElementById('reset').addEventListener('click',()=>{
-    localStorage.clear();
-})
 
-// document.addEventListener("keyup", function(event){
-//     if(event.keyCode == 13) {
-
-//         if(task) {
-
-//         }
-//     }
-// })
+  //remove button
+function remover(element, id) {
+    element.parentElement.remove();
+    HABITLIST[id].remover = true;
+    save();
+}
